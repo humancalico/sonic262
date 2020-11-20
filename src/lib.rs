364f1e0e55d549ce88183b6358968cc38998e7d0
Akshat Agarwal <humancalico@disroot.org>
2020-11-20 16:53:50 +0530

    store includes in a hashmap(dashmap)
    this commit introduces the dashmap crate which stores the includes
    a hashmap so that they are read only once and only when they are
    needed. done for performance reasons so that the file reads are
    minimal but I don't think there's any noticable difference since
    file reading is already pretty fast.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use color_eyre::eyre::Result;
 // TODO use a color library with no runtime dependency like yansi
 use colored::Colorize;
+use dashmap::DashMap;
 use rayon::prelude::*;
 use serde_yaml::Value::Sequence;
 use std::fs;
@@ -35,13 +36,13 @@ impl Diagnostics {
     }
 }
 
+// TODO get rid of this function. This is just a match wrapper around generate_final_file_to_test
 pub fn generate_and_run(
     file_to_test: &PathBuf,
-    files_to_add: Vec<PathBuf>,
+    include_contents: String,
     diagnostics: Arc<Diagnostics>,
-    must_include: Arc<&str>
 ) {
-    match generate_final_file_to_test(file_to_test, files_to_add, must_include) {
+    match generate_final_file_to_test(file_to_test, include_contents) {
         Ok(file_to_run) => match spawn_node_process(file_to_run) {
             Ok(exit_status) => {
                 if exit_status.success() {
@@ -99,29 +100,42 @@ pub fn get_serde_value(frontmatter_str: &str) -> serde_yaml::Result<serde_yaml::
     serde_yaml::from_str(frontmatter_str)
 }
 
-pub fn get_include_paths(
+pub fn get_contents(
+    includes_map: &DashMap<String, String>,
     includes_value: &serde_yaml::Value,
     include_path_root: &PathBuf,
-) -> serde_yaml::Result<Vec<PathBuf>> {
-    let includes: Vec<String> = serde_yaml::from_value(includes_value.clone())?;
-    let mut include_paths: Vec<PathBuf> = vec![];
-    includes.into_iter().for_each(|include| {
-        include_paths.push(include_path_root.join(include));
-    });
-    Ok(include_paths)
+) -> Result<String> {
+    let mut includes: Vec<String> = vec!["assert".to_string(), "sta".to_string()];
+    includes.append(&mut serde_yaml::from_value(includes_value.clone())?);
+    let includes_clone = includes.clone();
+    for include in includes {
+        let include_clone = include.clone();
+        if !(includes_map.contains_key(&include)) {
+            // read &include and store it in hashmap
+            includes_map.insert(
+                include,
+                fs::read_to_string(include_path_root.join(include_clone)).unwrap(),
+            );
+        }
+    }
+    let mut contents = String::new();
+    for include in includes_clone {
+        match includes_map.get(&include) {
+            Some(includes_value) => {
+                contents.push_str(&*includes_value);
+                contents.push('\n');
+            }
+            None => eprintln!("This should not have happened :|"),
+        }
+    }
+    Ok(contents)
 }
 
+// TODO combine this and get_contents()
 fn generate_final_file_to_test(
     file_to_test: &PathBuf,
-    files_to_add: Vec<PathBuf>,
-    must_include: Arc<&str>
+    mut contents: String,
 ) -> Result<NamedTempFile> {
-    let mut contents = String::new();
-    contents.push_str(&*must_include);
-    for file in files_to_add {
-        let file_contents = fs::read_to_string(file)?;
-        contents.push_str(&file_contents);
-    }
     let file_to_test_contents = fs::read_to_string(file_to_test)?;
     contents.push_str(&file_to_test_contents);
     let mut file = tempfile::Builder::new().suffix(".js").tempfile()?;
@@ -136,10 +150,15 @@ pub fn spawn_node_process(file: NamedTempFile) -> std::io::Result<ExitStatus> {
 }
 
 pub fn run_all(test_path: PathBuf, include_path: PathBuf) -> Result<()> {
-    let assert = fs::read_to_string(include_path.join("assert.js"))?;
-    let sta = fs::read_to_string(include_path.join("sta.js"))?;
-    let must_include = format!("{}\n{}\n", assert, sta);
-    let must_include = Arc::new(must_include.as_str());
+    let includes_map: DashMap<String, String> = DashMap::new();
+    includes_map.insert(
+        "assert".to_string(),
+        fs::read_to_string(include_path.join("assert.js"))?,
+    );
+    includes_map.insert(
+        "sta".to_string(),
+        fs::read_to_string(include_path.join("sta.js"))?,
+    );
 
     let files_to_test = walk(test_path)?;
     let diagnostics = Arc::new(Diagnostics::new());
@@ -149,19 +168,21 @@ pub fn run_all(test_path: PathBuf, include_path: PathBuf) -> Result<()> {
             Some(f) => match get_serde_value(&f) {
                 Ok(frontmatter_value) => match frontmatter_value.get("includes") {
                     Some(includes_value) => {
-                        match get_include_paths(includes_value, &include_path) {
-                            Ok(include_files) => {
-                                generate_and_run(&file, include_files, diagnostics.clone(), must_include.clone())
+                        match get_contents(&includes_map, includes_value, &include_path) {
+                            Ok(include_contents) => {
+                                generate_and_run(&file, include_contents, diagnostics.clone())
                             }
                             Err(e) => eprintln!("Not able to find {:?} | Err: {}", file, e),
                         }
                     }
-                    None => match get_include_paths(&Sequence([].to_vec()), &include_path) {
-                        Ok(include_files) => {
-                            generate_and_run(&file, include_files, diagnostics.clone(), must_include.clone())
+                    None => {
+                        match get_contents(&includes_map, &Sequence([].to_vec()), &include_path) {
+                            Ok(include_contents) => {
+                                generate_and_run(&file, include_contents, diagnostics.clone())
+                            }
+                            Err(e) => eprintln!("Not able to find {:?} | Err: {}", file, e),
                         }
-                        Err(e) => eprintln!("Not able to find {:?} | Err: {}", file, e),
-                    },
+                    }
                 },
                 Err(e) => eprintln!("Could not get serde value from frontmatter | Err: {}", e),
             },
@@ -188,15 +209,15 @@ pub fn run_all(test_path: PathBuf, include_path: PathBuf) -> Result<()> {
     Ok(())
 }
 
-//fn store_includes_in_hashmap() -> Result<()> {}
-
 #[cfg(test)]
 mod test {
     use super::Diagnostics;
     use static_assertions::assert_impl_all;
+    use std::marker::Send;
+    use std::marker::Sync;
 
     #[test]
     fn test() {
-        assert_impl_all!(Diagnostics: std::marker::Send, std::marker::Sync);
+        assert_impl_all!(Diagnostics: Send, Sync);
     }
 }
